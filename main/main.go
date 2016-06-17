f608c9e546916b9a14130927f12c9266a6e977ed
zhengchun <zhengchunster@gmail.com>
2016-06-17 09:12:52 +0800

    examples

--- main/main.go
+++ main/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/zhengchun/selector"
 )
 
-func main() {
+func html() {
+	//HTML
 	s := `
 	<html>
     <head></head>
@@ -27,8 +29,42 @@ func main() {
     </body>
     </html>`
 	doc, _ := selector.ParseHtml(strings.NewReader(s))
-	var nodes = selector.SelectHtmlNodes(doc, "//a[5 mod 2]")
+	var nodes = selector.SelectHtmlNodes(doc, "//a")
 	for _, node := range nodes {
 		fmt.Println(selector.HtmlNodeInnerText(node))
 	}
+}
+
+func xml() {
+	//XML
+	s := `<?xml version="1.0" encoding="UTF-8"?>
+<bookstore>
+<book>
+  <title lang="en">Harry Potter</title>
+  <price>29.99</price>
+</book>
+<book>
+  <title lang="en">Learning XML</title>
+  <price>39.95</price>
+</book>
+</bookstore>`
+	doc, _ := selector.ParseXml(strings.NewReader(s))
+	var nodes = selector.SelectXmlNodes(doc, "//book")
+	for _, node := range nodes {
+		fmt.Println(selector.XmlNodeInnerText(node))
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		html()
+		wg.Done()
+	}()
+	go func() {
+		xml()
+		wg.Done()
+	}()
+	wg.Wait()
 }
